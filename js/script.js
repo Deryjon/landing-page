85a589f7c65a57323e42c5559aa0e7a97726d1a4
Dery Jon <yusupoviskandar1005@gmail.com>
2024-07-06 16:28:51 +0500

    finish: finished test task (2024-07-06, 16:28)

--- js/script.js
+++ js/script.js
@@ -15,33 +15,59 @@ document.addEventListener('DOMContentLoaded', () => {
     const noButton = document.getElementById('noButton');
   
     const updateQuestion = () => {
-      questionText.textContent = questions[currentQuestionIndex];
-    };
+        if (currentQuestionIndex < questions.length) {
+          const questionElement = document.querySelector('.question');
+          
+          // Анимация исчезания блока вопроса
+          questionElement.classList.remove('open');
+          setTimeout(() => {
+            // Обновление текста вопроса
+            questionText.textContent = questions[currentQuestionIndex];
+            
+            // Анимация появления блока вопроса
+            questionElement.classList.add('open');
+          }, 500); // Увеличили задержку до 500 миллисекунд
+        }
+      };
+      
   
     const updateProgress = () => {
-      const progressPercentage = currentQuestionIndex / questions.length * 100;
-      progressElement.style.width = `${progressPercentage}%`;
-      progressElement.textContent = `${Math.round(progressPercentage)}%`;
-    };
+        const progressPercentage = (currentQuestionIndex / questions.length) * 100;
+        progressElement.style.width = `${progressPercentage}%`;
+        progressElement.textContent = `${Math.round(progressPercentage)}%`;
+      };
+      
+    // startButton.addEventListener('click', () => {
+    
+    //   currentQuestionIndex = 0;
+    //   updateQuestion();
+    //   updateProgress();
+    // });
   
     startButton.addEventListener('click', () => {
-      aboutSection.style.display = 'none';
-      testSection.style.display = 'block';
-      currentQuestionIndex = 0;
-      updateQuestion();
-      updateProgress();
-    });
-  
-    const showNextQuestion = () => {
-      if (currentQuestionIndex < questions.length - 1) {
-        currentQuestionIndex++;
+        aboutSection.style.display = 'none';
+        testSection.style.display = 'block';
+        testSection.classList.add('fade-in');
+        currentQuestionIndex = 0;
         updateQuestion();
         updateProgress();
-      }
-    };
-  
-    yesButton.addEventListener('click', showNextQuestion);
-    noButton.addEventListener('click', showNextQuestion);
+      });
+      
+      const showNextQuestion = () => {
+        if (currentQuestionIndex < questions.length - 1) {
+          currentQuestionIndex++;
+          updateQuestion();
+          updateProgress();
+        } else if (currentQuestionIndex === questions.length - 1) {
+          // Завершение последнего вопроса
+          currentQuestionIndex++;
+          updateProgress();
+        }
+      };
+      
+      yesButton.addEventListener('click', showNextQuestion);
+      noButton.addEventListener('click', showNextQuestion);
+
   
     updateQuestion();
     updateProgress();
